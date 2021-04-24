5bb5ff6f2f5b3b4c183eccd35efea41d964a651a
Maci <maci1011@facebook.com>
2021-04-24 18:29:01 +0200

    general edits

--- Resources/public/js/jscript.js
+++ Resources/public/js/jscript.js
@@ -1,9 +1,65 @@
 
 (function($){
 
+var maciMailer = function (options) {
+
+	var _defaults = {},
+
+		mailId, mailData, subscribers
+
+	;
+
+	_obj = {
+
+	getMailData: function() {
+		$.ajax({
+			type: 'GET',
+			data: {},
+			url: '/mcm/view/mails/mail/show/' + mailId,
+			success: function(d,s,x) {
+				mailData = d.item;
+				$('#actions').html('');
+				$('<h3/>').text('Mail: ' + d.item.id).appendTo('#actions');
+				$('<span/>').text('Name: ' + d.item.name).appendTo('#actions');
+				$('<span/>').text('Subject: ' + d.item.subject).appendTo('#actions');
+				$('<span/>').text('Index Position: ' + d.item.data.toIndex).appendTo('#actions');
+				$('<span/>').text('Send List Length: ' + d.item.data.sendList.length).appendTo('#actions');
+			}
+		});
+	},
+
+	getSubribersList: function() {
+		$.ajax({
+			type: 'GET',
+			data: {},
+			url: '/mcm/view/mails/subscriber/list',
+			success: function(d,s,x) {
+				$('#content').html('');
+				$('<h3/>').text('Subribers List').appendTo('#content');
+				var list = $('<ul/>').appendTo('#content');
+				for (var i = 0; i < d.list.length; i++) {
+					$('<span/>').text(d.list[i].name).appendTo(list)
+						.wrap('<li/>').parent().attr('data-id', d.list[i].id);
+				}
+				subscribers = d.list;
+			}
+		});
+	}
+
+	}; // _obj
+
+	mailId = $('#mail').attr('data-id');
+	_obj.getMailData();
+	_obj.getSubribersList();
+
+	return _obj;
+
+}
+
+
 $(document).ready(function(e) {
 
-	
+	maciMailer();
 
 });
 
